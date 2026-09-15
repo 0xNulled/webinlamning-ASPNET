38f256cdcfd59573fc52ee8075ad8ab669812c56
Joel Lindström <test@fedora.com>
2026-09-15 20:58:14 +0200

    Lade till fil för att lägga till default och testing värden

---
+++ TestInstances.cs
@@ -0,0 +1,13 @@
+namespace Metallix;
+using Metallix.Models;
+
+
+//Dummydata för test
+public class Initialization{
+    public List<Band> BandList = new(){
+        new Band { Id = 1, Name = "Portrait Decay", Genre = "Experimental Black Metal", Active = true },
+        new Band { Id = 2, Name = "Demonic Science", Genre = "Melodic Death Metal", Active = true}
+    };
+
+    
+}
