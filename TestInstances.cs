0ef0be8951f5f34fd9d90278723ad835b3ed0fd1
Joel Lindström <test@fedora.com>
2026-09-23 22:04:29 +0200

    Wretched Lives har nu sitt eget album cover istället för III

--- TestInstances.cs
+++ TestInstances.cs
@@ -8,7 +8,7 @@ public class Initialization{
     public List<Album> AlbumList = new()
     {
         new Album { id = 1, albumName = "III", artist = "Portrait Decay", listeningStatus = true, imageURL = "/uploads/PortraitDecay-III.png" },
-        new Album { id = 2, albumName = "Wretched Lives", artist = "Demonic Science", listeningStatus = false, imageURL = "/uploads/PortraitDecay-III.png"},
+        new Album { id = 2, albumName = "Wretched Lives", artist = "Demonic Science", listeningStatus = false, imageURL = "/uploads/DemonicScience-WretchedLives.png"},
         new Album { id = 3, albumName = "Hivemind Narcosis", artist = "Thantifaxath", listeningStatus = false, imageURL = "/uploads/Thantifaxath-HiveMindNarcosis.png" }
     };
 }
